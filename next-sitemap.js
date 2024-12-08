412a52c56e3faf33e7bc14c57c3bf638d6f213a4
vats <vatsalmangukiya9003@gmail.com>
2024-12-08 20:07:25 +0530

    sitemap lib added

---
+++ next-sitemap.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://snapsave.tech', // Replace with your domain
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  };
+  
