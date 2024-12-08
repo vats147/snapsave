0c549b784e051142caa147ec58d8449199e51e41
vats <vatsalmangukiya9003@gmail.com>
2024-12-08 20:27:42 +0530

    sitemap and robots addded

---
+++ src/app/sitemap.xml/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>https://www.snapsave.tech/</loc>
+      <lastmod>2024-12-08T00:00:00+01:00</lastmod>
+      <priority>1.0</priority>
+    </url>
+    <!-- Add more URLs as needed -->
+  </urlset>`;
+
+  return new NextResponse(sitemapXml, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
+}
